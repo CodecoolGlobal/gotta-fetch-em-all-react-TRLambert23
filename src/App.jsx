b71337a2e0ee220d77938f7d7d705e821498a85e
Patrik Bodis <142505568+bodispatrik1995@users.noreply.github.com>
2024-01-15 14:23:32 +0100

    step0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
+const pokemonLocationUrl = 'https://pokeapi.co/api/v2/location'
+const [locations, setLocations] = useState(null)
+
+useEffect(() =>{
+async function fetchData(){
+  const data = await fetch(pokemonLocationUrl)
+  const pokemonLocations = await data.json();
+  setLocations(pokemonLocations)
+}
+fetchData()
+}, [])
+
 
 
   return (
     <>
-      
+     
     </>
   )
 }
