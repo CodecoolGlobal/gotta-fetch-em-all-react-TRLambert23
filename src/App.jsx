546992a9db94a45aa567ca16f096bb40cf6362de
Patrik Bodis <142505568+bodispatrik1995@users.noreply.github.com>
2024-01-17 23:04:58 +0100

    select caracter kesz

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LoadEnemyPokemon from './components/EnemyPokemon';
 import HandleLocations from './components/HandleLocations'
 import Header from './components/Header'
 import { useState } from 'react';
-
+import SelectCaracter from './SelectCaracter';
 function App() {
   
  const [chosenLocation, setChosenLocation] = useState()
@@ -15,7 +15,9 @@ function toggleLocation (url) {
   return (
     <>
       <h1><Header/></h1>
-      {chosenLocation ? <LoadEnemyPokemon url={chosenLocation}/> : <HandleLocations OnPick={toggleLocation}/>}
+      
+      {chosenLocation ? <LoadEnemyPokemon url={chosenLocation}/> : <HandleLocations OnPick={toggleLocation} />}
+      
     </>
   ) 
 }
--- src/SelectCaracter.jsx
+++ src/SelectCaracter.jsx
@@ -1,41 +1,60 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-function selectCaracter(){
-    const usersPokemon = [
-        "https://pokeapi.co/api/v2/pokemon/bulbasaur",
-        "https://pokeapi.co/api/v2/pokemon/charizard",
-        "https://pokeapi.co/api/v2/pokemon/poliwhirl"
-    ]
-const [pokemond1, setPokemon1] = useState(undefined);
-const [pokemon2, setPokemon2] = useState(undefined);
-const [pokemon3, setPokemon3] = useState(undefined)
+function selectCaracter() {
+  const usersPokemon = [
+    "https://pokeapi.co/api/v2/pokemon/bulbasaur",
+    "https://pokeapi.co/api/v2/pokemon/charizard",
+    "https://pokeapi.co/api/v2/pokemon/poliwhirl",
+  ];
 
+  const [pokemon1, setPokemon1] = useState(undefined);
+  const [pokemon2, setPokemon2] = useState(undefined);
+  const [pokemon3, setPokemon3] = useState(undefined);
 
+  const [selectedCaracter, setSelectedCarater] = useState("");
 
-
-    const [selectedCaracter, setSelectedCarater] = useState('');
-    const handleClick = (e) =>{
-        setSelectedCarater(e.target.url)
-    }
-useEffect(() => {
+  useEffect(() => {
     async function fetchData(url) {
       const response = await fetch(url);
       const data = await response.json();
-      
+      return data;
     }
-      fetchData(selectCaracter);
+    async function setPokemons() {
+      setPokemon1(await fetchData(usersPokemon[0]));
+      setPokemon2(await fetchData(usersPokemon[1]));
+      setPokemon3(await fetchData(usersPokemon[2]));
+    }
+    setPokemons();
   }, []);
 
-
-  return
-<>
-<div>
-    <h1 onClick={handleClick}  >Bulbasaur</h1>
-     </div>
-</>
-
-
-
-
+  console.log(pokemon1);
+  return (
+    <>
+      <div>
+        <h3 id="pokemon1">{pokemon1 && pokemon1.name}</h3>
+        <img
+          id="enemy-pokemon-img"
+          src={pokemon1 && pokemon1.sprites["front_default"]}
+        />
+        <button>Select</button>
+      </div>
+      <div>
+        <h3 id="pokemon1">{pokemon2 && pokemon2.name}</h3>
+        <img
+          id="enemy-pokemon-img"
+          src={pokemon2 && pokemon2.sprites["front_default"]}
+        />
+        <button>Select</button>
+      </div>
+      <div>
+        <h3 id="pokemon1">{pokemon3 && pokemon3.name}</h3>
+        <img
+          id="enemy-pokemon-img"
+          src={pokemon3 && pokemon3.sprites["front_default"]}
+        />
+        <button>Select</button>
+      </div>
+    </>
+  );
 }
-export default selectCaracter
+export default selectCaracter;
