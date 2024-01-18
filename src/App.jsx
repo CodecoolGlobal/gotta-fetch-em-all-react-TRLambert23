a118a0eb1a760c6d647982fb552b361c2f82cd93
Patrik Bodis <142505568+bodispatrik1995@users.noreply.github.com>
2024-01-18 09:42:41 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import './App.css';
-import LoadEnemyPokemon from './components/EnemyPokemon';
-import HandleLocations from './components/HandleLocations'
-import Header from './components/Header'
-import { useState } from 'react';
-import SelectCaracter from './SelectCaracter';
+import "./App.css";
+import LoadEnemyPokemon from "./components/EnemyPokemon";
+import HandleLocations from "./components/HandleLocations";
+import Header from "./components/Header";
+import { useState } from "react";
+import SelectCaracter from "./components/SelectCaracter";
 function App() {
-  
- const [chosenLocation, setChosenLocation] = useState()
- 
-function toggleLocation (url) {
-  setChosenLocation(url)
-}
+  const [chosenLocation, setChosenLocation] = useState();
+
+  function toggleLocation(url) {
+    setChosenLocation(url);
+  }
 
   return (
     <>
-      <h1><Header/></h1>
-      
-      {chosenLocation ? <LoadEnemyPokemon url={chosenLocation}/> : <HandleLocations OnPick={toggleLocation} />}
-      
+      <h1>
+        <Header />
+      </h1>
+
+      {chosenLocation ? (
+        <LoadEnemyPokemon url={chosenLocation} />
+      ) : (
+        <HandleLocations OnPick={toggleLocation} />
+      )}
     </>
-  ) 
+  );
 }
 
 export default App;
-
--- src/components/EnemyPokemon.jsx
+++ src/components/EnemyPokemon.jsx
@@ -1,40 +1,50 @@
 import { useEffect, useState } from "react";
-import SelectCaracter from "../SelectCaracter";
-function LoadEnemyPokemon ({url}) {
-
-    const [pokemon, setPokemon] = useState()
-    const [selectButton, setSelectbutton] = useState(false)
-    function handleClick(){
-      setSelectbutton(true)
-    }
-    useEffect(() => {
-  async function displayPokemon() { //THIS
-    const data = await fetch(url);
-    const currentLocation = await data.json()
-    const randomAreaIndex = Math.floor(Math.random() * currentLocation.areas.length)
-    const areaUrl = currentLocation.areas[randomAreaIndex].url
-    const areaData = await fetch(areaUrl)
-    const area = await areaData.json()
-    const encounters = area['pokemon_encounters']
-    const randomPokemonIndex = Math.floor(Math.random() * encounters.length)
-    const encounterObject = encounters[randomPokemonIndex].pokemon
-    const encounterObjectUrl = encounterObject.url
-    const pokemonData = await fetch(encounterObjectUrl)
-    const pokemonObject = await pokemonData.json()
-    
-    setPokemon(pokemonObject)
+import SelectCharacter from "./SelectCaracter";
+function LoadEnemyPokemon({ url }) {
+  const [pokemon, setPokemon] = useState();
+  const [selectButton, setSelectbutton] = useState(false);
+  function handleClick() {
+    setSelectbutton(true);
   }
-  displayPokemon()
-    }, [])
+  useEffect(() => {
+    async function displayPokemon() {
+      //THIS
+      const data = await fetch(url);
+      const currentLocation = await data.json();
+      const randomAreaIndex = Math.floor(
+        Math.random() * currentLocation.areas.length
+      );
+      const areaUrl = currentLocation.areas[randomAreaIndex].url;
+      const areaData = await fetch(areaUrl);
+      const area = await areaData.json();
+      const encounters = area["pokemon_encounters"];
+      const randomPokemonIndex = Math.floor(Math.random() * encounters.length);
+      const encounterObject = encounters[randomPokemonIndex].pokemon;
+      const encounterObjectUrl = encounterObject.url;
+      const pokemonData = await fetch(encounterObjectUrl);
+      const pokemonObject = await pokemonData.json();
 
-    return (
-        <>{selectButton?(<SelectCaracter/>):(<div>
+      setPokemon(pokemonObject);
+    }
+    displayPokemon();
+  }, []);
+
+  return (
+    <>
+      {selectButton ? (
+        <SelectCharacter />
+      ) : (
+        <div>
           <h3 id="enemy-pokemon-name">{pokemon && pokemon.name}</h3>
-          <img id="enemy-pokemon-img" src={pokemon && pokemon.sprites['front_default']}/>
+          <img
+            id="enemy-pokemon-img"
+            src={pokemon && pokemon.sprites["front_default"]}
+          />
           <button onClick={handleClick}>Select your pokemon</button>
-      </div>)}</>
-        
-        )
+        </div>
+      )}
+    </>
+  );
 }
 
-export default LoadEnemyPokemon
+export default LoadEnemyPokemon;
