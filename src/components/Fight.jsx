012b8fbee799eaac88f98a06fef8103b936797bd
Lambert Szabo <142510288+TRLambert23@users.noreply.github.com>
2024-01-18 17:00:58 +0100

    fight started

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -1,11 +1,29 @@
-function Fight({enemy, userPokemon}){
-const enemyPokemon =enemy;
-const myPokemon = userPokemon;
-console.log(userPokemon)
-return(
-    <div>Hello</div>
-)
+function Fight({ enemy, userPokemon }) {
+  const enemyPokemon = enemy;
+  const myPokemon = userPokemon;
 
+  console.log(userPokemon);
+  
+  return (
+    <div>
+      <h3 id="enemy-fighter" key={enemyPokemon.name}>
+        {enemyPokemon.name}
+      </h3>
+      <img
+        id="enemy-fighter-img"
+        src={enemyPokemon && enemyPokemon.sprites["front_default"]}
+        alt={enemyPokemon && enemyPokemon.name}
+      />
+        <h3 id="user-fighter" key={myPokemon.name}>
+          {myPokemon.name}
+        </h3>
+        <img
+          id="user-fighter-img"
+          src={myPokemon && myPokemon.sprites["front_default"]}
+          alt={myPokemon && myPokemon.name}
+        />
+    </div>
+  );
 }
 
-export default Fight
+export default Fight;
