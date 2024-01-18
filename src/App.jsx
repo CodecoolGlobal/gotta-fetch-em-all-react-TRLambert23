d389013eb4f7c46842c188d8c1cf24d37708e250
Patrik Bodis <142505568+bodispatrik1995@users.noreply.github.com>
2024-01-18 10:10:02 +0100

    elirasok javitva

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import './App.css';
-import LoadEnemyPokemon from './components/EnemyPokemon';
-import HandleLocations from './components/HandleLocations'
-import Header from './components/Header'
-import { useState } from 'react';
-import SelectCaracter from './SelectCaracter';
+import "./App.css";
+import LoadEnemyPokemon from "./components/EnemyPokemon";
+import HandleLocations from "./components/HandleLocations";
+import Header from "./components/Header";
+import { useState } from "react";
+import SelectCharacter from "./components/SelectCharacter";
 function App() {
-  
- const [chosenLocation, setChosenLocation] = useState()
- 
-function toggleLocation (url) {
-  setChosenLocation(url)
-}
+  const [chosenLocation, setChosenLocation] = useState();
+
+  function toggleLocation(url) {
+    setChosenLocation(url);
+  }
 
   return (
-    
     <body>
-      <h1><Header/></h1>
-      {chosenLocation ? <LoadEnemyPokemon url={chosenLocation}/> : <HandleLocations OnPick={toggleLocation}/>}
+      <h1>
+        <Header />
+      </h1>
+      {chosenLocation ? (
+        <LoadEnemyPokemon url={chosenLocation} />
+      ) : (
+        <HandleLocations OnPick={toggleLocation} />
+      )}
     </body>
-  ) 
+  );
 }
 
 export default App;
-
